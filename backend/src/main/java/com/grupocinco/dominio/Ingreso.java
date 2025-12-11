e77f0dc196c933ece03cff40b1ba3e8ede729ec3
AxelKing <axelking.program@gmail.com>
2025-12-11 15:32:18 -0300

    Commit con problema a proposito

--- backend/src/main/java/com/grupocinco/dominio/Ingreso.java
+++ backend/src/main/java/com/grupocinco/dominio/Ingreso.java
@@ -41,7 +41,7 @@ public class Ingreso {
         if (enfermera == null)
             throw new IllegalArgumentException("Enfermera nula");
         if (informe == null || informe.isBlank())
-            throw new DatoMandatorioOmitidoException("Falta el dato Informe");
+            throw new DatoMandatorioOmitidoException("Falta el dato Informes");
 
         this.id = UUID.randomUUID();
         this.fechaIngreso = LocalDateTime.now();
